05557fc90d96e9ddd3ed0b0c1d8afd027dd7b671
YeEun Kim <uniye2260@gmail.com>
2022-07-09 14:34:09 +0900

    22.07.09

---
+++ Project15/Project15/Stock.h
@@ -0,0 +1,22 @@
+#ifndef STOCK
+#include<iostream>
+using namespace std;
+
+class Stock {
+private:
+	string name;
+	int shares;
+	float share_val;
+	double total_val;
+	void set_total() { total_val = shares * share_val; };
+
+public:
+	void acquire(string, int, float);
+	void buy(int, float);
+	void sell(int, float);
+	void update(float);
+	void show();
+	Stock();
+	~Stock();
+};
+#endif 
---
+++ Project15/Project15/func.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include "Stock.h"
+
+void Stock::acquire(string co, int n, float pr) {
+	name = co;
+	shares = n;
+	share_val = pr;
+	set_total();
+}
+void Stock::buy(int n, float pr) {
+	shares + n;
+	share_val = pr;
+	set_total();
+}
+void Stock::sell(int n, float pr) {
+	shares -= n;
+	share_val = pr;
+	set_total();
+}
+void Stock::update(float pr) {
+	share_val = pr;
+	set_total();
+}
+void Stock::show() {
+	cout << "회사 명: " << name << endl;
+	cout << "주식 수: " << shares << endl;
+	cout << "주가: " << share_val << endl;
+	cout << "주식 총 가치: " << total_val << endl;
+}
+
+Stock::Stock() { //생성자
+
+
+}
+
+Stock::~Stock() { //파괴자
+
+
+}
